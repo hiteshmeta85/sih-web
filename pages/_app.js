afad46d564be170f44a0897e882fc0a9cd6d1598
Hitesh Meta <meta.hitesh85@gmail.com>
2022-07-02 00:50:37 +0530

    Initial Project Setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import {ChakraProvider} from '@chakra-ui/react'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return (
+    <ChakraProvider>
+      <Head>
+        <title>Homebrew</title>
+        <meta charSet='utf-8'/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="description" content="Frontend for SIH Topic GS900"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
