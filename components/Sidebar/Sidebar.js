a066e3816127e6ff1fea259c41a63fd9beafbc95
Hitesh Meta <meta.hitesh85@gmail.com>
2022-07-04 00:34:47 +0530

    Add Sidebar icons

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -2,6 +2,7 @@ import {Flex, Icon, Link, List, ListIcon, ListItem, Text} from "@chakra-ui/react
 import NextLink from "next/link";
 import {SidebarItems} from "./SidebarItems";
 import {useRouter} from "next/router";
+import {BiCoffeeTogo} from "react-icons/bi";
 
 const Sidebar = () => {
 
@@ -17,15 +18,16 @@ const Sidebar = () => {
       color={'gray.300'}
       fontSize={'lg'}
     >
-      <Text
+      <Flex
         pl={6}
         pr={20}
         py={2}
         my={6}
+        alignItems='center'
       >
-        <Icon color={'gray.300'} mr={2}/>
-        Homebrew
-      </Text>
+        <Icon as={BiCoffeeTogo} color={'gray.300'} mr={2}/>
+        <Text as={'span'}>Homebrew</Text>
+      </Flex>
       <List>
         {SidebarItems.map((item, index) => {
           return (
@@ -40,7 +42,7 @@ const Sidebar = () => {
                   color={router.pathname === `${item.navLink}` ? 'black' : ''}
                   _hover={router.pathname !== `${item.navLink}` ? {bg: '#0F2E69'} : ''}
                 >
-                  <ListIcon color={router.pathname === `${item.navLink}` ? '#0B2656' : 'gray.300'}/>
+                  <ListIcon as={item.icon} color={router.pathname === `${item.navLink}` ? '#0B2656' : 'gray.300'}/>
                   {item.name}
                 </ListItem>
               </Link>
--- components/Sidebar/SidebarItems.js
+++ components/Sidebar/SidebarItems.js
@@ -1,18 +1,26 @@
+import {AiOutlineHome, AiOutlineProject} from "react-icons/ai";
+import {FiAlertTriangle} from "react-icons/fi";
+import {BiChat} from "react-icons/bi"
+
 export const SidebarItems = [
   {
     name: 'Dashboard',
-    navLink: '/dashboard'
+    navLink: '/dashboard',
+    icon: AiOutlineHome,
   },
   {
     name: 'Projects',
-    navLink: '/dashboard/projects'
+    navLink: '/dashboard/projects',
+    icon: AiOutlineProject,
   },
   {
-    name: 'Create Alert',
-    navLink: '/dashboard/create-alert'
+    name: 'Create Alerts',
+    navLink: '/dashboard/create-alert',
+    icon: FiAlertTriangle,
   },
   {
     name: 'Conversations',
-    navLink: '/dashboard/conversations'
+    navLink: '/dashboard/conversations',
+    icon: BiChat,
   },
 ]
