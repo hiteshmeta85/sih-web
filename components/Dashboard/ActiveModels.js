8c6b4ec984e5ea140b76c99806493a64e567bfaa
ShreyaMBelanekar42 <shreyaluvbelanekar@gmail.com>
2022-07-31 22:24:08 +0530

    add model images in dashboard section

--- components/Dashboard/ActiveModels.js
+++ components/Dashboard/ActiveModels.js
@@ -1,5 +1,11 @@
-import {Box, Flex, Text} from "@chakra-ui/react";
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
+import Image from "next/image";
+import audio from "./audio.png";
+import text from "./text.png";
+import classfication from "./classfication.png";
+import detection from "./detection.png";
+import image from "./image.png";
 
 const ActiveModels = () => {
   return (
@@ -7,21 +13,32 @@ const ActiveModels = () => {
       bg={"white"}
       borderRadius={"md"}
       border={"1px solid lightgray"}
-      h={'full'}
+      h={"full"}
       p={4}
     >
-      <Text fontSize={'lg'} fontWeight={'bold'} mb={2}>
+      <Text fontSize={"lg"} fontWeight={"bold"} mb={2}>
         Active Models
       </Text>
-      <Flex justifyContent={'space-between'}>
-        <Box p={5} bg={'green.400'} rounded={'full'}/>
-        <Box p={5} bg={'green.400'} rounded={'full'}/>
-        <Box p={5} bg={'green.400'} rounded={'full'}/>
-        <Box p={5} bg={'green.400'} rounded={'full'}/>
-        <Box p={5} bg={'green.400'} rounded={'full'}/>
+      <Flex justifyContent={'space-between'} gap={2}>
+          <Avatar p={{base: 2, md: 2, lg: 4}} bg={"green.400"} size={{base: 'sm', md: 'md', lg: 'md'}}>
+            <Image src={audio}/>
+          </Avatar>
+          <Avatar p={{base: 2, md: 2, lg: 4}} bg={"green.400"} size={{base: 'sm', md: 'md', lg: 'md'}}>
+            <Image src={text}/>
+          </Avatar>
+          <Avatar p={{base: 2, md: 2, lg: 4}} bg={"green.400"} size={{base: 'sm', md: 'md', lg: 'md'}}>
+            <Image src={classfication}/>
+          </Avatar>
+          <Avatar p={{base: 2, md: 2, lg: 4}} bg={"green.400"} size={{base: 'sm', md: 'md', lg: 'md'}}>
+            <Image src={detection}/>
+          </Avatar>
+          <Avatar p={{base: 2, md: 2, lg: 4}} bg={"green.400"} size={{base: 'sm', md: 'md', lg: 'md'}}>
+            <Image src={image}/>
+          </Avatar>
+        
       </Flex>
     </Box>
-  )
-}
+  );
+};
 
 export default ActiveModels;
