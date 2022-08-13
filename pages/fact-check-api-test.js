84150498caa5f4b54001e965d8f421a89ad0ac3a
Hitesh Meta <meta.hitesh85@gmail.com>
2022-08-13 20:28:57 +0530

    Test Fact Check API and Add Sample data

---
+++ pages/fact-check-api-test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import axios from "axios";
+import {Button} from "@chakra-ui/react";
+
+const FactCheckApiTest = () => {
+
+  const handleSubmit = (value) => {
+    axios.get(`https://factchecktools.googleapis.com/v1alpha1/claims:search?query=${encodeURIComponent(value)}&key=AIzaSyBsMtEU0B0j12lwr1aomRRCTHrrLKz5FbU`)
+      .then(r => console.log(r))
+      .catch(e => console.log(e))
+  }
+
+  return (
+    <div>
+      <Button onClick={()=> handleSubmit('assam flood')}>fact check</Button>
+    </div>
+  );
+};
+
+export default FactCheckApiTest;
