bf435c99eb62d53131f10d9cab633b238a55ccf4
Hitesh Meta <meta.hitesh85@gmail.com>
2022-08-17 12:36:21 +0530

    Celery Test Success

--- pages/celery-test.js
+++ pages/celery-test.js
@@ -4,41 +4,41 @@ import axios from "axios";
 const CeleryTest = () => {
 
   const [celeryKeys, setCeleryKeys] = useState({
-    "twitter_task": "3cc71676-8ec7-44fb-b6ad-8c9b07b07500",
-    "facebook_task": "461a1865-6866-49a8-ac05-f76f9adf1882"
+    "twitter_task_id": "8e4d39aa-a8aa-4406-9eb9-77538e296c80",
+    "facebook_task_id": "a8771e71-7f6b-495c-af77-5db7d3b94cc5"
   })
   const [twitterData, setTwitterData] = useState([])
   const [facebookData, setFacebookData] = useState([])
+  const [instagramData, setInstagramData] = useState([])
   const [didWeGetTaskIds, setDidWeGetTaskIds] = useState(false)
 
-  const newFn = () => {
+  const newFn = async () => {
     if (localStorage.getItem('twitterKeys')) {
+      console.log("working on twitter keys")
       let twitterKeys = localStorage.getItem('twitterKeys')
       twitterKeys = JSON.parse(twitterKeys)
       const getTwitterData = async () => {
         try {
           const response = await axios.get(`http://127.0.0.1:8000/homebrew/api/task/${twitterKeys[0]}`)
           if (response) {
-            if (response.data.status === 'SUCCESS') {
-              console.log(response)
-              setTwitterData(response.data.result)
+            if (response.data.status === 'SUCCESS' || 'FAILURE') {
+              setTwitterData([...twitterData, ...response.data.result])
               return true
             }
           }
         } catch (err) {
-          console.log(err);
           return false
         }
       };
-      //twitterKeys.shift()
       if (twitterKeys.length > 0) {
-        const response = getTwitterData()
-        console.log('initial', twitterKeys)
+        const response = await getTwitterData()
+        console.log("twitter response", response)
+        console.log('initial', twitterKeys.length)
         if (response === true) {
           twitterKeys.shift()
-          console.log('twitter success', twitterKeys)
+          console.log('twitter success')
         } else {
-          console.log("twitter failed")
+          console.log("twitter failed for :", twitterKeys[0])
         }
       }
       if (twitterKeys.length === 0) {
@@ -47,29 +47,30 @@ const CeleryTest = () => {
         localStorage.setItem('twitterKeys', JSON.stringify(twitterKeys))
       }
     } else if (localStorage.getItem('facebookKeys')) {
+      console.log("working on facebook keys")
       let facebookKeys = localStorage.getItem('facebookKeys')
       facebookKeys = JSON.parse(facebookKeys)
       const getFacebookData = async () => {
         try {
           const response = await axios.get(`http://127.0.0.1:8000/homebrew/api/task/${facebookKeys[0]}`)
           if (response) {
-            if (response.data.status === 'SUCCESS') {
-              setFacebookData(response.data.result)
+            if (response.data.status === 'SUCCESS' || 'FAILURE') {
+              setFacebookData([...facebookData, ...response.data.result])
               return true
+            } else {
+              return false
             }
           }
         } catch (err) {
-          console.log(err);
           return false
         }
       };
       if (facebookKeys.length > 0) {
-        console.log("1 length")
-        const response = getFacebookData()
+        const response = await getFacebookData()
         if (response === true) {
           facebookKeys.shift()
           console.log('facebook success', facebookKeys)
-        }  else {
+        } else {
           console.log("facebook failed")
         }
       }
@@ -79,6 +80,7 @@ const CeleryTest = () => {
         localStorage.setItem('twitterKeys', JSON.stringify(facebookKeys))
       }
     } else if (Object.keys(celeryKeys).length === 0) {
+      console.log("getting celery keys")
       const getCeleryKeys = async () => {
         try {
           const response = await axios.get(`http://127.0.0.1:8000/homebrew/api/new/projects/39/text`)
@@ -91,45 +93,80 @@ const CeleryTest = () => {
       };
       getCeleryKeys()
     } else if (Object.keys(celeryKeys).length !== 0 && didWeGetTaskIds === false) {
+      console.log("getting tasks")
       const getTwitterTaskId = async () => {
         try {
-          const response = await axios.get(`http://127.0.0.1:8000/homebrew/api/task/${celeryKeys.twitter_task}`)
+          const response = await axios.get(`http://127.0.0.1:8000/homebrew/api/task/${celeryKeys.twitter_task_id}`)
           if (response) {
-            if (response.data.status === 'SUCCESS') {
+            console.log(response.data)
+            if (response.data.status === 'SUCCESS' || 'FAILURE') {
+              console.log(response.data.result)
               localStorage.setItem('twitterKeys', JSON.stringify(response.data.result))
               return true
+            } else {
+              return false
             }
           }
         } catch (err) {
-          console.log(err);
           return false
         }
       };
       const getFacebookTaskId = async () => {
         try {
-          const response = await axios.get(`http://127.0.0.1:8000/homebrew/api/task/${celeryKeys.facebook_task}`)
+          const response = await axios.get(`http://127.0.0.1:8000/homebrew/api/task/${celeryKeys.facebook_task_id}`)
           if (response) {
-            if (response.data.status === 'SUCCESS') {
+            if (response.data.status === 'SUCCESS' || 'FAILURE') {
               localStorage.setItem('facebookKeys', JSON.stringify(response.data.result))
               return true
+            } else {
+              return false
             }
           }
         } catch (err) {
-          console.log(err);
           return false
         }
       };
-      if ((!localStorage.getItem('twitterKeys') || !(localStorage.getItem('facebookKeys')))) {
-        const twitterResponse = getTwitterTaskId()
-        const facebookResponse = getFacebookTaskId()
-        if (twitterResponse && facebookResponse) {
+      const getInstagramTaskId = async () => {
+        try {
+          const response = await axios.get(`http://127.0.0.1:8000/homebrew/api/task/${celeryKeys.instagram_task_id}`)
+          if (response) {
+            if (response.data.status === 'SUCCESS' || 'FAILURE') {
+              localStorage.setItem('instagramKeys', JSON.stringify(response.data.result))
+              return true
+            } else {
+              return false
+            }
+          }
+        } catch (err) {
+          return false
+        }
+      };
+
+      if ((celeryKeys.hasOwnProperty("twitter_task_id") && localStorage.getItem('twitterKeys') === null)
+        || (celeryKeys.hasOwnProperty("facebook_task_id") && localStorage.getItem('facebookKeys') === null)
+        || (celeryKeys.hasOwnProperty("instagram_task_id") && localStorage.getItem('instagramKeys') === null)) {
+        let twitterResponse, facebookResponse, instagramResponse
+        if (celeryKeys.hasOwnProperty("twitter_task_id")) {
+          twitterResponse = await getTwitterTaskId()
+        }
+        if (celeryKeys.hasOwnProperty("facebook_task_id")) {
+          facebookResponse = await getFacebookTaskId()
+        }
+        if (celeryKeys.hasOwnProperty("instagram_task_id")) {
+          instagramResponse = await getInstagramTaskId()
+        }
+        if ((celeryKeys.hasOwnProperty("twitter_task_id") ? twitterResponse !== false : true) &&
+          (celeryKeys.hasOwnProperty("facebook_task_id") ? facebookResponse !== false : true) &&
+          (celeryKeys.hasOwnProperty("instagram_task_id") ? instagramResponse !== false : true)) {
           setDidWeGetTaskIds(true)
         }
       }
     } else {
-      console.log("Key Not Found")
+      console.log("Keys Not Found: Dead End")
     }
   }
+
+  console.log("Twitter Data", twitterData, "Facebook Data", facebookData, "Instagram Data", instagramData)
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -138,8 +175,6 @@ const CeleryTest = () => {
 
     return () => clearInterval(interval);
   }, [newFn]);
-
-  console.log({'celeryKeys': celeryKeys}, {'twitterData': twitterData}, {'facebookData': facebookData})
 
   return (
     <div>
