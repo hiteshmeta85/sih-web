0d29ea2db932eabb6ced647b6f3aba06842517e9
ShreyaMBelanekar42 <shreyaluvbelanekar@gmail.com>
2022-08-19 23:07:31 +0530

    Add Live Demo Test Components

--- pages/dashboard/live-demo/audio-test.js
+++ pages/dashboard/live-demo/audio-test.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React from "react";
+import LiveDemoLayout from "./_layout";
+import {
+  Box,
+  Flex,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+
+import { Highlights } from "../../../components/LiveDemo/Highlights";
+import { NerData } from "../../../components/LiveDemo/NerData";
+import { Labels } from "../../../components/LiveDemo/Labels";
+import { UploadFile } from "../../../components/LiveDemo/UploadFile";
 
 const AudioTest = () => {
   return (
-    <div>
-      AT
-    </div>
+    <LiveDemoLayout heading={"Live Demo | Audio"}>
+      {/* todo: add to urls */}
+      <Flex flexDir={"column"} gap={4} p={4}>
+        <UploadFile />
+        <Highlights />
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
+          <Box
+            rounded={"lg"}
+            border={"1px solid lightgray"}
+            padding={5}
+            boxShadow={"lg"}
+          >
+            <Text fontWeight={"semibold"}>Audio To Text</Text>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
+              nesciunt ratione repellat sunt suscipit, totam. Adipisci cum
+              laudantium voluptatem! Consequuntur culpa illum magni,
+              necessitatibus porro praesentium quam quas? Sit, voluptatem?
+              Accusantium at deserunt exercitationem impedit magni minus rerum
+              sit temporibus. In inventore labore laborum ratione saepe! Ad
+              aliquid culpa distinctio dolorem eligendi eum laborum, maiores
+              quae, sapiente soluta vero voluptates.
+            </Text>
+          </Box>
+        </SimpleGrid>
+        <SimpleGrid columns={1} spacing={4}>
+          <NerData />
+          <Labels />  
+        </SimpleGrid>
+      </Flex>
+    </LiveDemoLayout>
   );
 };
 
--- pages/dashboard/live-demo/index.js
+++ pages/dashboard/live-demo/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React from "react";
 import LiveDemoLayout from "./_layout";
-import {Box} from "@chakra-ui/react";
+import {
+  Flex,
+  Textarea,
+  Button,
+} from "@chakra-ui/react";
+import {Formik} from "formik";
+// import textUploadSchema from "../../../lib/schemas/";
+import { Highlights } from "../../../components/LiveDemo/Highlights";
+import { NerData } from "../../../components/LiveDemo/NerData";
+import { Labels } from "../../../components/LiveDemo/Labels";
 
 const Index = () => {
   return (
-    <LiveDemoLayout>
+    <LiveDemoLayout >
       {/* todo: add to urls */}
-      Live Demo <Box p={1} className={'blink'} bg={'red'}/>
+      <Flex flexDir={'column'} gap={4} p={4}>
+        <Textarea
+            variant="outline"
+            placeholder="Enter your text here"
+            maxW={"600px"}
+            size="lg"
+            bg="white"
+          />
+      <Button
+          type={"submit"}
+          bg={"blackAlpha.800"}
+          textColor={"white"}
+          _hover={{ bg: "blackAlpha.700" }}
+          _active={{ bg: "blackAlpha.800" }}
+          px={12}
+          w={'100px'}
+        >
+          Submit
+        </Button>
+        <Highlights />
+        <NerData />
+        <Labels />
+      </Flex>
     </LiveDemoLayout>
   );
 };
