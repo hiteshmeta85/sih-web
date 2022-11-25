50cf3c4e20e0ba2c1b45a6c8540b6789fa53a1af
Hitesh Meta <meta.hitesh85@gmail.com>
2022-11-25 23:54:29 +0530

    Move routes to constants folder

---
+++ constants/routes.js
@@ -0,0 +1,153 @@
+export const routes = [
+  {
+    title: 'Event Flow',
+    pages: [
+      {
+        title: 'Index',
+        url: '/event-flow/',
+      },
+      {
+        title: 'Raw Data Extraction',
+        url: '/event-flow/raw-data-extraction',
+      },
+      {
+        title: 'Binary Text Classification',
+        url: '/event-flow/binary-text-classification',
+      },
+      {
+        title: 'Multi label text classification',
+        url: '/event-flow/multi-label-text-classification',
+      },
+      {
+        title: 'Binary Image Classification',
+        url: '/event-flow/binary-image-classification',
+      },
+      {
+        title: 'Multi label image classification',
+        url: '/event-flow/multi-label-image-classification',
+      },
+      {
+        title: 'Image Object Detection',
+        url: '/event-flow/image-object-detection',
+      },
+      {
+        title: 'Binary Video Classification',
+        url: '/event-flow/binary-video-classification',
+      },
+      {
+        title: 'Video Object Detection',
+        url: '/event-flow/video-object-detection',
+      },
+      {
+        title: 'Voice Analysis',
+        url: '/event-flow/voice-analysis',
+      },
+      {
+        title: 'Geolocation Marking',
+        url: '/event-flow/geolocation-marking',
+      },
+      {
+        title: 'Conclusion',
+        url: '/event-flow/conclusion',
+      },
+    ]
+  },
+  {
+    title: 'Live Demo',
+    pages: [
+      {
+        title: 'Index',
+        url: '/dashboard/live-demo',
+      },
+    ]
+  },
+  {
+    title: 'Create Project',
+    pages: [
+      {
+        title: 'Index',
+        url: '/dashboard/create-project',
+      },
+    ]
+  },
+  {
+    title: 'Dashboard',
+    pages: [
+      {
+        title: 'Index',
+        url: '/dashboard',
+      },
+      {
+        title: 'Projects',
+        url: '/dashboard/projects',
+      },
+      {
+        title: 'Alerts',
+        url: '/dashboard/alerts',
+      },
+      {
+        title: 'Account',
+        url: '/dashboard/account',
+      },
+    ]
+  },
+  {
+    title: 'Landing Pages',
+    pages: [
+      {
+        title: 'Index',
+        url: '/',
+      },
+      {
+        title: 'Contact',
+        url: '/contact',
+      },
+      {
+        title: 'News',
+        url: '/news',
+      },
+      {
+        title: 'Alerts',
+        url: '/alerts',
+      },
+      {
+        title: 'Tweets',
+        url: '/tweets',
+      },
+      {
+        title: 'Active Accounts',
+        url: '/active-accounts',
+      },
+    ]
+  },
+  {
+    title: 'Auth',
+    pages: [
+      {
+        title: 'Login',
+        url: '/auth/login',
+      },
+      {
+        title: 'Register',
+        url: '/auth/register',
+      },
+    ]
+  },
+  {
+    title: 'Test Files',
+    pages: [
+      {
+        title: 'Cors Test',
+        url: '/new-feature-test/cors',
+      },
+      {
+        title: 'Fast Check API Test',
+        url: '/new-feature-test/fact-check-api',
+      },
+      {
+        title: 'React Table Test',
+        url: '/new-feature-test/react-table',
+      },
+    ]
+  },
+]
